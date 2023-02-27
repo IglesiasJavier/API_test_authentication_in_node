d3a9e333128da5d68f6493430be5224500f05622
Javier <j.iglesias.zam@gmail.com>
2023-02-27 15:49:17 -0600

    middleware to validate tokens

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as productsController from '../controllers/products.controller';
+import { verifyToken } from "../middlewares";
 
 const router = Router();
 
 router.get('/',productsController.index);
-router.post('/',productsController.store);
+router.post('/',verifyToken,productsController.store);
 router.get('/:productId',productsController.show);
-router.put('/:productId',productsController.update);
-router.delete('/:productId',productsController.del);
+router.put('/:productId',verifyToken,productsController.update);
+router.delete('/:productId',verifyToken,productsController.del);
 
 export default router;
